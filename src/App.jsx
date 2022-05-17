dbaa3db384e4535d8a6a10fb38b61f1128db12c0
andreseichi <andre_seichi@hotmail.com>
2022-05-17 12:46:24 -0300

    pagina de assentos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { Sessions } from './pages/Sessions';
+import { Seats } from './pages/Seats';
 
 import { GlobalStyle } from './styles/global';
 
@@ -13,6 +14,9 @@ export function App() {
         <Route path="/" element={<Home />} />
         <Route path="sessoes">
           <Route path=":idFilme" element={<Sessions />} />
+        </Route>
+        <Route path="assentos">
+          <Route path=":idSessao" element={<Seats />} />
         </Route>
       </Routes>
     </>
