f12b7830146e570eaa3aeddb4d82751f9fae0e28
andreseichi <andre_seichi@hotmail.com>
2022-05-18 13:33:17 -0300

    botão

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,5 @@
+import { Container } from './styles';
+
+export function Button({ children }) {
+  return <Container>{children}</Container>;
+}
---
+++ src/components/Button/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+  background: #e8833a;
+  border-radius: 3px;
+  border: none;
+
+  font-size: 18px;
+  line-height: 21px;
+  text-align: center;
+  letter-spacing: 0.04em;
+
+  color: #ffffff;
+
+  width: 225px;
+  height: 42px;
+
+  cursor: pointer;
+`;
